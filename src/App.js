fe7cc394aca3c4bc6c9656937a3b1f0ba0b4f5fe
Selena Siri <selenasiri@gmail.com>
2020-11-05 18:31:43 -0500

    reducer and store

--- src/App.js
+++ src/App.js
@@ -4,14 +4,39 @@ import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
+
 // redux stuff
+
+import {createStore} from 'redux';
+
+
+
+
+
+// initial store
+const initialStore = {
+  count: 78
+};
+// reducer 
+function reducer(state, action) {
+  console.log({state, action}); // action is undefined, so for here redux passes a default action
+  if(action.type === "DECREASE") {
+    console.log("hey it actually worked")
+    return { count: state.count -1} //creates a new object without mutating the old one
+  }
+  return state; // return the state before the update
+}
+// store
+const store = createStore(reducer, initialStore); // but now the initialState is 'count'.
+store.dispatch({type: "DECREASE"})
+console.log(store.getState());
 
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()}/>
       <CartContainer cart={cartItems} />
     </main>
   );
